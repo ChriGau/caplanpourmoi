e8f4af1f472a88876e1cc49b12c00dc1682aab52
Axel Martin <martin.axe@live.fr>
2017-08-26 16:56:10 +0200

    Create Planning Model missing team_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170825165920) do
+ActiveRecord::Schema.define(version: 20170826145518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20170825165920) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_constraints_on_user_id", using: :btree
+  end
+
+  create_table "plannings", force: :cascade do |t|
+    t.integer  "week_number"
+    t.integer  "year"
+    t.integer  "status"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "role_users", force: :cascade do |t|
