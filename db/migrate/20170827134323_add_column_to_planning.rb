a7d2124240bc42cafddbeab6edb67294686e1abc
pierre favario <p.favario@gmail.com>
2017-08-29 14:39:35 +0200

    schema udate

--- db/migrate/20170827134323_add_column_to_planning.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToPlanning < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :plannings, :team, foreign_key: true
-  end
-end
--- db/migrate/20170828104518_remove_column_from_plannings.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnFromPlannings < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :plannings, :team_id, :integer
-  end
-end
