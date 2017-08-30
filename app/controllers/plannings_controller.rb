6e04b2456308579548cc501a6ce116430e41ca70
ChriGau <christelle.gaudron@laposte.net>
2017-08-30 11:23:03 +0200

    refacto JS du skeleton.html

--- app/controllers/plannings_controller.rb
+++ app/controllers/plannings_controller.rb
@@ -8,7 +8,6 @@ class PlanningsController < ApplicationController
   end
 
   def show
-
   end
 
   def skeleton
@@ -21,52 +20,24 @@ class PlanningsController < ApplicationController
     @slots_array_nice = []
     @slots_array = []
     @slots.each do |slot|
-      # compter le nombre de people sur le slot = slots avec même heure de début et de fin et role
-        nombre = 0
-        @slots.each do |s|
-          if (s.start_at == slot.start_at and s.end_at == slot.end_at and s.role_id == slot.role_id)
-            nombre += 1
-          end
-        end
-        # si @slots_array ne contient pas déjà ce slot (soit cpt = 0)
-        cpt = 0
-        @slots_array_nice.each do |sl|
-          if @slots_array_nice != []
-            if sl != nil
-              if (sl[:start].to_date == slot.start_at.to_date and sl[:start].hour == slot.start_at.hour and sl[:end].to_date == slot.end_at.to_date and sl[:role_id] == slot.role_id)
-                # attention, je ne fais pas le matching sur les minutes
-                cpt +=1
-              end
-            end
-          end
-        end
+      nombre = count_people_on_slot(@slots, slot) # nb people sur le slot = slots avec même heure de début et de fin et role
+        cpt = count_presence_in_array(@slots, slot) # si >0, le slot est déjà présent dans l'array
+        a= {
+              id:  slot.id,
+              start:  slot.start_at,
+              end: slot.end_at,
+              title: Role.find_by_id(slot.role_id).name, # nom du role
+              role_id: slot.role_id, # nom du role
+              nombre: nombre, # nombre de users nécessaires au groupement de slots
+              created_at: slot.created_at,
+              updated_at: slot.updated_at,
+              color: Role.find_by_id(slot.role_id).role_color
+              }
         if cpt == 0
           # construire le NICE hash
-          a= {
-              id:  slot.id,
-              start:  slot.start_at,
-              end: slot.end_at,
-              title: Role.find_by_id(slot.role_id).name, # nom du role
-              role_id: slot.role_id, # nom du role
-              nombre: nombre, # nombre de users nécessaires au groupement de slots
-              created_at: slot.created_at,
-              updated_at: slot.updated_at,
-              color: Role.find_by_id(slot.role_id).role_color
-              }
           @slots_array_nice << a
-          end
+        end
           # construire le BASIC hash
-          a= {
-              id:  slot.id,
-              start:  slot.start_at,
-              end: slot.end_at,
-              title: Role.find_by_id(slot.role_id).name, # nom du role
-              role_id: slot.role_id, # nom du role
-              nombre: nombre, # nombre de users nécessaires au groupement de slots
-              created_at: slot.created_at,
-              updated_at: slot.updated_at,
-              color: Role.find_by_id(slot.role_id).role_color
-              }
           @slots_array << a
   end
   @slots_array_nice.to_json
@@ -96,6 +67,32 @@ end
 
   def set_planning
     @planning = Planning.find(params[:id])
+  end
+
+  def count_people_on_slot(slots, slot)
+    nombre = 0
+    slots.each do |s|
+      if (s.start_at == slot.start_at and s.end_at == slot.end_at and s.role_id == slot.role_id)
+        nombre += 1
+      end
+    end
+    return nombre
+  end
+
+  def count_presence_in_array(slots, slot)
+    # si cpt > 0, 1 slot similaire est déjà présent dans @slots_array_nice
+      cpt = 0
+      @slots_array_nice.each do |sl|
+        if @slots_array_nice != []
+          if sl != nil
+            if (sl[:start].to_date == slot.start_at.to_date and sl[:start].hour == slot.start_at.hour and sl[:end].to_date == slot.end_at.to_date and sl[:role_id] == slot.role_id)
+              # attention, je ne fais pas le matching sur les minutes
+              cpt +=1
+            end
+          end
+        end
+      end
+      return cpt
   end
 
 end
