76cff26b45d43131856b75f634187d5d8e80750b
Bruno Guers <bruno.guers@gmail.com>
2017-08-30 16:27:00 +0200

    slidebar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,8 @@
+
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require moment
 //= require fullcalendar
+//= require sly
 //= require_tree .
---
+++ app/assets/javascripts/planning.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  var options = {
+    horizontal: 1,
+    itemNav: 'basic',
+    speed: 300,
+    mouseDragging: 1,
+    touchDragging: 1
+  };
+  $('#planning-list').sly(options);
+});
