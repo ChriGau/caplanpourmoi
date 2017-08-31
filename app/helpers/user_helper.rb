ad1d56d8174b50e8ae4ff82eec170a7db6af9bee
Bruno Guers <bruno.guers@gmail.com>
2017-08-31 16:01:26 +0200

    seeds with images

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -3,11 +3,15 @@ module UserHelper
     color = user.roles[0].role_color if user.roles.any?
     color ||= "black"
 
-    if user.profile_picture?
-      cl_image_tag user.profile_picture.path, class: "user-avatar-index", style: "border: solid 3px #{color}"
-    else
-      image_tag "http://placehold.it/30x30", class: "user-avatar-index", style: "border: solid 3px #{color}"
+    options = {
+      class: "user-avatar-index",
+      style: "border: solid 3px #{color}",
+    }
 
+    if user.profile_picture?
+      cl_image_tag user.profile_picture.path, options
+    else
+      image_tag "http://placehold.it/30x30", options
     end
   end
 end
