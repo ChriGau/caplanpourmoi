31a8452d2c2d35a8c12beedf76c77de10fa1f38e
Bruno Guers <bruno.guers@gmail.com>
2017-09-03 13:10:12 +0200

    triangles background color

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,14 +9,17 @@ module ApplicationHelper
       @set_status1 = "active"
       @set_status2 = "disabled"
       @set_status3 = "disabled"
+      @triangle_position = "calc(16% - 70px)"
     when "users"
       @set_status1 = "completed"
       @set_status2 = "active"
       @set_status3 = "disabled"
+      @triangle_position = "calc(50% - 70px)"
     else
       @set_status1 = "completed"
       @set_status2 = "completed"
       @set_status3 = "active"
+      @triangle_position = "calc(84% - 70px)"
     end
   end
 
