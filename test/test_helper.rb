2a6104bd74afa4041d4d3dea6e8b1a48bf738af3
Bruno Guers <bruno.guers@gmail.com>
2017-12-14 19:57:06 +0100

    minitestereporter'

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,10 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'minitest/reporters'
+# rubocop:disable ColonMethodCall
+Minitest::Reporters::use! [Minitest::Reporters::ProgressReporter.new]
+# rubocop:enable ColonMethodCall
 
 class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
