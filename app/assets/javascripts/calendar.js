3592434692f3e8b8f6028d97f0c80a0b8cafe6cf
ChriGau <christelle.gaudron@laposte.net>
2018-04-04 13:16:17 +0200

    css modal-events

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
   $('.draggable').data('duration', '03:00');
   // when clicking on its 'Cancel' button
   $(".cancel-button").click(function(){
-    $(".modal_events").modal('hide');
+    $(".modal-events").modal('hide');
     // 1. get border thickness of Event back to normal
     $(".fc-time-grid-event").css('border-width', '0px','box-shadow', '0px 0px 3.5px -1px white inset', 'padding', '2px' , 'padding-top', '7px','padding', '3px');
     // 2. reload page
