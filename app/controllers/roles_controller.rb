c837964dee622ab60c33b117c826aea1090d12d6
ChriGau <christelle.gaudron@laposte.net>
2018-04-06 17:33:51 +0200

    style'

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -2,23 +2,50 @@ class RolesController < ApplicationController
   def new
     @role = Role.new
     @color_collection = []
-    Role.color_list.each { |value| @color_collection << value[:name_fr] }
+    Role.color_list.each { |key, value| @color_collection << value[:name_fr] }
   end
 
   def create
-    @role = Role.new(params_role)
+    color_hexadecimal = Role.color_list.select{ |k,v| v[:name_fr] == params_role[:role_color] }.values.first[:code]
+    @role = Role.new(role_color: color_hexadecimal, name: params_role[:name])
     if @role.save
-      redirect_to plannings_path, notice: 'un nouveau role a été ajouté'
+      redirect_to plannings_path
     else
       render :new
     end
   end
 
-  def edit; end
+  def edit
+    @role = Role.find(params[:id])
+    @role_color = @role.role_color
+    @color_collection = []
+    Role.color_list.each { |key, value| @color_collection << value[:name_fr] }
+  end
 
-  def update; end
+  def update
+    @role = Role.find(params[:id])
+    # color_role needs to be the hexadecimal color code VS color name_fr
+    color_hexadecimal = Role.color_list.select{ |k,v| v[:name_fr] == params_role[:role_color] }.values.first[:code]
+    @role.role_color = color_hexadecimal
+    @role.name = params_role[:name]
+    if @role.save
+      redirect_to plannings_path, notice: 'Le role a été modifié'
+    else
+      render :update
+    end
+  end
 
-  def destroy; end
+  def destroy
+    @role = Role.find(params[:id])
+    if !@role.role_users.count.positive?
+      if @role.destroy
+        redirect_to plannings_path
+      else
+        flash[:error] = 'Impossible de supprimer le role - dépendances'
+        redirect_to plannings_path
+      end
+    end
+  end
 
   private
 
