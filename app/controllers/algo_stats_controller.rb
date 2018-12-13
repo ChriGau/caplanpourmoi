d12c6874c4d4a3be9d6ca91ce2cbd8e145a473ff
ChriGau <christelle.gaudron@laposte.net>
2018-12-13 17:28:01 +0100

    step1

--- app/controllers/algo_stats_controller.rb
+++ app/controllers/algo_stats_controller.rb
@@ -2,14 +2,16 @@
 class AlgoStatsController < ApplicationController
 
 def show_statistics_algo
-  @algo_stat = AlgoStat.last
-  @compute_solutions = ComputeSolution.last(50).select{|c| c.status != "pending"}.last(15)
-  @table_rows = []
-  @compute_solutions.each do |compute_solution|
-    @table_rows << compute_solution.build_row_for_statistics_display
+  unless AlgoStat.count.zero?
+    @algo_stat = AlgoStat.last
+    @compute_solutions = ComputeSolution.last(50).select{|c| c.status != "pending"}.last(15)
+    @table_rows = []
+    @compute_solutions.each do |compute_solution|
+      @table_rows << compute_solution.build_row_for_statistics_display
+    end
+    @bar_chart_rows = @algo_stat.calculations_per_week(@compute_solutions, "01/06/2018".to_date)
+    @curve_chart_rows =
   end
-  @bar_chart_rows = @algo_stat.calculations_per_week(@compute_solutions, "01/06/2018".to_date)
-  @curve_chart_rows = curve_chart_rows
 end
 
 def reload_statistics
